bade103fcc93c1db06ddc00dd347616cfb4fbcab
Janne Raty <jraty@student.hive.fi>
2020-11-12 14:24:34 +0200

    day03

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($_GET["action"] == "set")
+	setcookie($_GET["name"], $_GET["value"], time() + (86400 * 30), '/'); // 86400 = 1 day
+if ($_GET["action"] == "get")
+{
+	if ($_COOKIE[$_GET["name"]])
+		echo $_COOKIE[$_GET["name"]] . "\n";
+}
+if ($_GET["action"] == "del")
+	setcookie($_GET["name"], $_GET["value"], NULL, -1, '/');
+?>
