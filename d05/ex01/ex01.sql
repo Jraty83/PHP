e7dd8173b9acb67fae9e10e1d6ed9c86fc600f39
Janne Raty <jraty@student.hive.fi>
2020-11-17 19:52:28 +0200

    minor fixes

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS ft_table;
 CREATE TABLE ft_table (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`login` varchar(8) DEFAULT 'toto' NOT NULL,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`login` VARCHAR(8) DEFAULT 'toto' NOT NULL,
 	`group` ENUM('staff', 'student', 'other') NOT NULL,
-	creation_date date NOT NULL
+	creation_date DATE NOT NULL
 );
